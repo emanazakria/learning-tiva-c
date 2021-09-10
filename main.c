f6eb5105422f9b744a21d553588cf4832e6777a8
emanazakria <arafaaeman@gmail.com>
2021-09-10 22:58:42 +0200

    modified main.c

--- main.c
+++ main.c
@@ -13,13 +13,13 @@ void PortF_Init(void){
   SYSCTL_RCGCGPIO_R |= 0x00000020;                // 1) activate clock for Port F
   while ((SYSCTL_PRGPIO_R & 0x20)== 0) {};	      // check if clock is activated 
   GPIO_PORTF_LOCK_R = GPIO_LOCK_KEY;              // 2) unlock GPIO Port F
-	GPIO_PORTF_CR_R = 0x1F;                         // allow changes to PF4:PF0
+  GPIO_PORTF_CR_R = 0x1F;                         // allow changes to PF4:PF0
   GPIO_PORTF_AMSEL_R = 0x00;                      // 3) disable analog on PF
-	GPIO_PORTF_PCTL_R = 0x00000000;                 // 4) PCTL GPIO on PF4:PF0
-	GPIO_PORTF_DIR_R = 0x0E;                        // 5) PF4,PF0 input, PF3:PF1 output
-	GPIO_PORTF_AFSEL_R = 0x00;                      // 6) disable alt funct on PF7:PF0
+  GPIO_PORTF_PCTL_R = 0x00000000;                 // 4) PCTL GPIO on PF4:PF0
+  GPIO_PORTF_DIR_R = 0x0E;                        // 5) PF4,PF0 input, PF3:PF1 output
+  GPIO_PORTF_AFSEL_R = 0x00;                      // 6) disable alt funct on PF7:PF0
   GPIO_PORTF_PUR_R = 0x11;                        // enable pull-up on PF0 and PF4
-	GPIO_PORTF_DEN_R = 0x1F;                        // 7) enable digital I/O on PF4:PF0
+  GPIO_PORTF_DEN_R = 0x1F;                        // 7) enable digital I/O on PF4:PF0
 	}
 	
 	
@@ -35,7 +35,7 @@ void PortF_Output(uint32_t data){ // write PF3:PF1 outputs
 
 
 int main(void){ uint32_t status;
-  PortF_Init();              // initialize PF0 and PF4 and make them inputs(built-in switches)
+  PortF_Init();                // initialize PF0 and PF4 and make them inputs(built-in switches)
 	                           // make PF3:PF1 outputs (PF3:PF1 built-in LEDs)
   while(1){
     status = PortF_Input();
